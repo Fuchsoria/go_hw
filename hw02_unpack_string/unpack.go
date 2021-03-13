fa988a64bf19a001473bece6306bd11ec5dea876
German <sehenqq@gmail.com>
2021-03-13 22:54:35 +0100

    Fix linting

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,6 +10,22 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+func validateRunes(i int, r rune, nextRune rune, inOneRune rune) (bool, error) {
+	if i == 0 && unicode.IsDigit(r) {
+		return false, ErrInvalidString
+	}
+
+	if r != 92 && unicode.IsDigit(nextRune) && unicode.IsDigit(inOneRune) {
+		return false, ErrInvalidString
+	}
+
+	if (!unicode.IsLetter(r) && r != 92 && !unicode.IsDigit(r)) && unicode.IsDigit(nextRune) {
+		return false, ErrInvalidString
+	}
+
+	return true, nil
+}
+
 func Unpack(str string) (string, error) {
 	if len(str) == 0 {
 		return "", nil
@@ -25,8 +41,7 @@ func Unpack(str string) (string, error) {
 			continue
 		}
 
-		var nextRune rune
-		var inOneRune rune
+		var nextRune, inOneRune rune
 
 		if len(str)-1 >= i+1 {
 			nextRune, _ = utf8.DecodeRuneInString(string(str[i+1]))
@@ -36,45 +51,38 @@ func Unpack(str string) (string, error) {
 			inOneRune, _ = utf8.DecodeRuneInString(string(str[i+2]))
 		}
 
-		if i == 0 && unicode.IsDigit(r) {
-			return "", ErrInvalidString
+		_, validatingError := validateRunes(i, r, nextRune, inOneRune)
+
+		if validatingError != nil {
+			return "", validatingError
 		}
 
-		if r != 92 && unicode.IsDigit(nextRune) && unicode.IsDigit(inOneRune) {
-			return "", ErrInvalidString
-		}
-
-		if (!unicode.IsLetter(r) && r != 92 && !unicode.IsDigit(r)) && unicode.IsDigit(nextRune) {
-			return "", ErrInvalidString
-		}
-
-		if unicode.IsLetter(nextRune) && r == 48 {
-			continue
-		} else if r == 92 && unicode.IsDigit(nextRune) && unicode.IsDigit(inOneRune) {
+		switch {
+		case r == 92 && unicode.IsDigit(nextRune) && unicode.IsDigit(inOneRune):
 			digit, _ := strconv.Atoi(string(inOneRune))
 
 			result.WriteString(strings.Repeat(string(nextRune), digit))
 
 			skipNext = true
-		} else if r == 92 && unicode.IsDigit(nextRune) {
+		case r == 92 && unicode.IsDigit(nextRune):
 			result.WriteRune(nextRune)
 
 			skipNext = true
-		} else if r == 92 && nextRune == 92 && unicode.IsDigit(inOneRune) {
+		case r == 92 && nextRune == 92 && unicode.IsDigit(inOneRune):
 			digit, _ := strconv.Atoi(string(inOneRune))
 
 			result.WriteString(strings.Repeat(string(r), digit))
 
 			skipNext = true
-		} else if r == 92 && nextRune == 92 {
+		case r == 92 && nextRune == 92:
 			result.WriteRune(92)
 
 			skipNext = true
-		} else if unicode.IsDigit(nextRune) {
+		case unicode.IsDigit(nextRune):
 			digit, _ := strconv.Atoi(string(nextRune))
 
 			result.WriteString(strings.Repeat(string(r), digit))
-		} else if unicode.IsLetter(r) {
+		case unicode.IsLetter(r):
 			result.WriteRune(r)
 		}
 	}
