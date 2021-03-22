3f5f118f982e7b9a1b32b76f915236458ccb079c
German <fuchsoria@protonmail.ch>
2021-03-22 23:30:37 +0100

    Ädd small fixes

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -12,7 +12,7 @@ var (
 	clearWordRegex = regexp.MustCompile(`[^a-zA-Zа-яА-Яё]+`)
 )
 
-type Word struct {
+type word struct {
 	key   string
 	count int
 }
@@ -21,7 +21,7 @@ func clearWord(word string) (string, error) {
 	str := clearWordRegex.ReplaceAllString(word, "")
 
 	if len(str) == 0 {
-		return "", errors.New("Word is empty")
+		return "", errors.New("word is empty")
 	}
 
 	return strings.ToLower(str), nil
@@ -29,8 +29,8 @@ func clearWord(word string) (string, error) {
 
 func Top10(text string) []string {
 	wordsByString := wordRegex.FindAllString(text, -1)
-	maps := make(map[string]int)
-	words := []Word{}
+	maps := map[string]int{}
+	words := []word{}
 	result := []string{}
 
 	for _, word := range wordsByString {
@@ -40,17 +40,11 @@ func Top10(text string) []string {
 			continue
 		}
 
-		_, isWordExist := maps[word]
-
-		if isWordExist {
-			maps[word]++
-		} else {
-			maps[word] = 1
-		}
+		maps[word]++
 	}
 
 	for k, v := range maps {
-		words = append(words, Word{key: k, count: v})
+		words = append(words, word{key: k, count: v})
 	}
 
 	sort.Slice(words, func(a, b int) bool {
