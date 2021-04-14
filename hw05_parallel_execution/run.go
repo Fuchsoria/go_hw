e510b99c1f5677187c44555b6575b0a62dd3ac05
German <sehenqq@gmail.com>
2021-04-14 20:47:06 +0200

    Add structure changes

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,26 +3,40 @@ package hw05parallelexecution
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func watcher(tasks chan Task, errors chan bool, limit chan bool, killsignal chan bool, maxErrors int, wg *sync.WaitGroup, once *sync.Once) {
+func watcher(tasks chan Task, errorsCount *int64, limit chan struct{}, killsignal chan struct{}, maxErrors int, wg *sync.WaitGroup, once *sync.Once, once2 *sync.Once) {
+	closeSignal := func() { once.Do(func() { close(killsignal) }) }
+	closeLimit := func() { once2.Do(func() { close(limit) }) }
+
 	for {
 		select {
 		case task := <-tasks:
-			if len(errors) >= maxErrors {
-				once.Do(func() { close(limit) })
-			} else {
-				err := task()
-				if err != nil {
-					errors <- true
-				}
+			wg.Add(1)
+			err := task()
+			if err != nil {
+				atomic.AddInt64(errorsCount, 1)
+			}
+			wg.Done()
+
+			if errors := int(*errorsCount); errors >= maxErrors {
+				closeSignal()
+				closeLimit()
+
+				return
 			}
 
-			wg.Done()
+			if len(tasks) == 0 {
+				closeSignal()
+
+				return
+			}
+
 		case <-killsignal:
 			return
 		}
@@ -32,24 +46,26 @@ func watcher(tasks chan Task, errors chan bool, limit chan bool, killsignal chan
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	ch := make(chan Task, len(tasks))
-	errors := make(chan bool, m+n)
-	killsignal := make(chan bool)
-	limit := make(chan bool)
+	killsignal := make(chan struct{})
+	limit := make(chan struct{})
+
+	var errorsCount int64
+
 	once := sync.Once{}
+	once2 := sync.Once{}
 	wg := sync.WaitGroup{}
 
-	defer close(errors)
 	defer close(ch)
-	defer close(killsignal)
 
 	for i := 0; i < n; i++ {
-		go watcher(ch, errors, limit, killsignal, m, &wg, &once)
+		go watcher(ch, &errorsCount, limit, killsignal, m, &wg, &once, &once2)
 	}
 
 	for _, t := range tasks {
-		wg.Add(1)
 		ch <- t
 	}
+
+	<-killsignal
 
 	wg.Wait()
 
